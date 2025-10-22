fa48bbd01a43b1a66c3629c60277b2193066e51b
goncharov_oi <goncharovoi@yandex.ru>
2025-10-22 21:17:11 +0300

    Ranme ModelWithNominalModeRotary to ModelRotaryNominalMode. Add ModelLinear and ModelLinearNominalMode to support linear motors.

--- pmsm.py
+++ pmsm.py
@@ -68,36 +68,17 @@ def flux_from_nominal_generator_mode(R, L, N, vn, Pn, In, Un, sign = 1, J = np.n
         # return result
         return Fm, PointDQ(Id, Iq), PointDQ(Ud, Uq), stable
 
-class ModelRotary(DeductorBaseNamed): 
+class _ModelBase(DeductorBaseNamed):
+    ''' Incomplete model common for Rotary and Linear motors. '''
     _ATTRIBUTES = [
         BaseAttribute('L', 'H', 'Motor iductance as it present in model.', groups=['inductance']),
+        BaseAttribute('L2', 'H', '2-nd motor iductance harmonic.', groups=['inductance']),
         BaseAttribute('R', 'Ohm', 'Resistance, phase resistance (resistance between zero point and phase).', groups=['resistanse']),
         BaseAttribute('Fm', 'Wb', 'Rotor flux linkage', groups=['flux']),
-        BaseAttribute('N', None, 'Number of poles pairs.', groups=['poles']),
-        BaseAttribute('J', 'kg m^2', 'Rotor inertia.', groups=['inertia']),
         AliasAttribute('Rph', 'R', groups=['resistanse']),
-        AliasAttribute('n_pole_pairs', 'N', groups=['poles']),
         ScaledAliasAttribute('Lph', 2.0/3.0, 'L', 'H', 'Measured phase inductnce (between zero point and phase).', groups=['inductance']),
         ScaledAliasAttribute('Lll', 2.0, 'L', 'H', 'Measured line to line inductnce (between two phases).', groups=['inductance']),
         ScaledAliasAttribute('Rll', 2.0, 'R', 'Ohm', 'Measured line to line resistance  (between two phases)', groups=['resistanse']),
-        ScaledAliasAttribute('n_poles', 2, 'N', None, 'Number of poles', groups=['poles']),
-        DerivedAttribute('Kemf', 'Vs', 'Meashured back EMF constant: speed in rad/s to phase voltage amplitude.', 
-                         lambda Fm, N: N*Fm, groups=['flux']),
-        DerivedAttribute('Kt', 'Hm/A', 'Torque constant.', 
-                         lambda Fm, N: 3/2*N*Fm, groups=['flux']),
-        ScaledAliasAttribute('Kemf_llrms_rpm', 2*np.pi/60 * np.sqrt(3/2), 'Kemf', 'V/rpm', 
-                             'Meashured back EMF constant: speed in rpms to rms line-to-line voltage.', groups=['flux']),
-        ScaledAliasAttribute('Kemf_rpm', 2*np.pi/60, 'Kemf', 'V/rpm', 
-                             'Meashured back EMF constant: speed in rpms to phase voltage amplitud.', groups=['flux']),
-    ]
-    _DERIVATE_RULES = [
-        DerivateRule('Fm', lambda N, Kt: Kt/(3/2*N)),
-        DerivateRule('Fm', lambda N, Kemf: Kemf/N),
-    ]
-
-class ModelRotaryDQ(ModelRotary): 
-    _ATTRIBUTES = [
-        BaseAttribute('L2', 'H', '2-nd motor iductance harmonic.', groups=['inductance']),
         DerivedAttribute('Ld', 'H', 'Inductance along direct axis.', 
                          lambda L, L2: L + L2, groups=['inductance']),
         DerivedAttribute('Lq', 'H', 'Inductance along quadrature axis.', 
@@ -106,29 +87,83 @@ class ModelRotaryDQ(ModelRotary):
     _DERIVATE_RULES = [
         DerivateRule('L', lambda Ld, Lq: (Ld + Lq)/2),
         DerivateRule('L2', lambda Ld, Lq: (Ld - Lq)/2),
+        DerivateRule('L2', lambda L, Ld: Ld - L),
+        DerivateRule('L2', lambda L, Lq: L - Lq),
     ]
-
+    
     def __init__(self, *args, **kwargs):
-        super(ModelRotaryDQ, self).__init__(*args, **kwargs)
+        super(_ModelBase, self).__init__(*args, **kwargs)
         # set default L2
         if np.isnan(self.L2):
-            self.L2 = 0
+            self.L2 = 0.0
 
-class ModelWithNominalModeRotary(ModelRotaryDQ): 
+class ModelRotary(_ModelBase):
     _ATTRIBUTES = [
-        BaseAttribute('Un', 'V', 'Rated phase voltage amplitude (beeween phase and zero point).'),
-        BaseAttribute('In', 'A', 'Rated current amplitude (on phase line).'),
-        BaseAttribute('vn', 'rad/s', 'Rated speed.'),
-        BaseAttribute('Tn', 'Nm', 'Rated torque.'),
-        AliasAttribute('rated_speed', 'vn', groups=['rated_speed','rated']),
-        AliasAttribute('rated_torque', 'Tn', groups=['rated_torque','rated']),
+        BaseAttribute('N', None, 'Number of poles pairs.', groups=['poles']),
+        BaseAttribute('J', 'kg m^2', 'Rotor inertia.', groups=['inertia']),
+        AliasAttribute('n_pole_pairs', 'N', groups=['poles']),
+        ScaledAliasAttribute('n_poles', 2, 'N', None, 'Number of poles', groups=['poles']),
+        DerivedAttribute('Kemf', 'Vs', 'Meashured back EMF constant: speed in rad/s to phase voltage amplitude.', 
+                         lambda Fm, N: N*Fm, groups=['flux']),
+        DerivedAttribute('Kt', 'Hm/A', 'Torque constant: current amplitude to force.', 
+                         lambda Fm, N: 3/2*N*Fm, groups=['flux']),
+        ScaledAliasAttribute('Kemf_llrms_rpm', 2*np.pi/60 * np.sqrt(3/2), 'Kemf', 'V/rpm', 
+                             'Meashured back EMF constant: speed in rpms to rms line-to-line voltage.', groups=['flux']),
+        ScaledAliasAttribute('Kemf_rpm', 2*np.pi/60, 'Kemf', 'V/rpm', 
+                             'Meashured back EMF constant: speed in rpms to phase voltage amplitud.', groups=['flux']),
+        ScaledAliasAttribute('Kemf_ll', np.sqrt(3), 'Kemf', 'Vs', 
+                             'Meashured back EMF constant: speed in rad to line-to-linr voltage amplitude.', groups=['flux']),
+    ]
+    _DERIVATE_RULES = [
+        DerivateRule('Fm', lambda N, Kt: Kt/(3/2*N)),
+        DerivateRule('Fm', lambda N, Kemf: Kemf/N),
+    ]
+
+class ModelLinear(_ModelBase):
+    _ATTRIBUTES = [
+        BaseAttribute('tau', 'm', 'Pole pitch, distance between two poles.', groups=['poles']),
+        BaseAttribute('m', 'kg', 'Moving part mass.', groups=['inertia']),
+        AliasAttribute('pole_pitch', 'tau', groups=['poles']),
+        ScaledAliasAttribute('pole_pair_pitch', 2, 'pole_pitch', 'm', 'Number of poles', groups=['poles']),
+        DerivedAttribute('Kemf', 'Vs/m', 'Meashured back EMF constant: speed in m/s to phase voltage amplitude.', 
+                         lambda Fm, tau: np.pi/tau*Fm, groups=['flux']),
+        DerivedAttribute('Kt', 'H/A', 'Torque constant: current amplitude to force.', 
+                         lambda Fm, tau: 3/2*np.pi/tau*Fm, groups=['flux']),
+        ScaledAliasAttribute('Kemf_llrms', np.sqrt(3/2), 'Kemf', 'Vs/m', 
+                             'Meashured back EMF constant: speed in m/s to rms line-to-line voltage.', groups=['flux']),
+        ScaledAliasAttribute('Kemf_ll', np.sqrt(3), 'Kemf', 'Vs/m', 
+                             'Meashured back EMF constant: speed in m/s to line-to-linr voltage amplitude.', groups=['flux']),
+        # compatibility with rotary model
+        DerivedAttribute('N', '1/m', 'Compatibility: pi/tau ', 
+                         lambda tau: np.pi/tau, groups=['compatibility']),
+        DerivedAttribute('J', 'kg', 'Compatibility: m ', 
+                         lambda m: m, groups=['compatibility']),
+    ]
+    _DERIVATE_RULES = [
+        DerivateRule('Fm', lambda tau, Kt: Kt/(3/2*np.pi/tau)),
+        DerivateRule('Fm', lambda tau, Kemf: Kemf/(np.pi/tau)),
+    ]
+
+class _NominalModeBase(DeductorBaseNamed):
+    ''' Incomplete nominal mode specification common for Rotary and Linear motors. '''
+    _ATTRIBUTES = [
+        BaseAttribute('Un', 'V', 'Rated phase voltage amplitude (beeween phase and zero point).', groups=['rated_voltage','rated']),
+        BaseAttribute('In', 'A', 'Rated current amplitude (on phase line).', groups=['rated_current','rated']),
         ScaledAliasAttribute('rated_ac_voltage', np.sqrt(3/2), 'Un', 'V', 'Rated 3-phase rms voltage', groups=['rated_voltage', 'rated']),
         ScaledAliasAttribute('Un_rms', np.sqrt(1/2), 'Un', 'V', 'Rated phase rms voltage'),
         ScaledAliasAttribute('rated_dc_voltage', np.sqrt(3), 'Un', 'V', 'DC bus volatge', groups=['rated_voltage','rated']),
         ScaledAliasAttribute('In_rms', 1.0/np.sqrt(2), 'In', 'A', 'Rated rms phase current (on phase line)'),
-        ScaledAliasAttribute('rated_speed_rpm', 30/np.pi, 'vn', 'rpm', 'Rated speed.', groups=['rated_speed','rated']),
         AliasAttribute('rated_current', 'In_rms', groups=['rated_current','rated']),
         AliasAttribute('rated_ac_phase_voltage', 'Un_rms', groups=['rated_voltage','rated']),
+    ]
+
+class ModelRotaryNominalMode(ModelRotary, _NominalModeBase): 
+    _ATTRIBUTES = [
+        BaseAttribute('vn', 'rad/s', 'Rated speed.'),
+        BaseAttribute('Tn', 'Nm', 'Rated torque.'),
+        AliasAttribute('rated_speed', 'vn', groups=['rated_speed','rated']),
+        AliasAttribute('rated_torque', 'Tn', groups=['rated_effort','rated']),
+        ScaledAliasAttribute('rated_speed_rpm', 30/np.pi, 'vn', 'rpm', 'Rated speed.', groups=['rated_speed','rated']),
         DerivedAttribute('Pn', 'W', 'Rated power', lambda vn, Tn: vn*Tn),
         DerivedAttribute('fn', 'Hz', 'Rated frequency (electrical).', 
                          lambda N, vn: N*vn/(2*np.pi)),
@@ -140,6 +175,30 @@ class ModelWithNominalModeRotary(ModelRotaryDQ):
         DerivateRule('vn', lambda N, fn: 2*np.pi*fn/N),
         DerivateRule('Tn', lambda vn, Pn: Pn/vn),
         DerivateRule('Fm', lambda R, L, N, vn, Pn, In, Un: flux_from_nominal_motor_mode(R, L, N, vn, Pn, In, Un, sign = 1, J = np.nan)[0]),
+    ]
+    _VALIDATORS = [
+        Validator(lambda Un_rms, In_rms, Pn: 3*Un_rms*In_rms > Pn, 'Mechanical power output must not be greater then electrical power input')
+     ]
+
+class ModelLinearNominalMode(ModelLinear, _NominalModeBase): 
+    _ATTRIBUTES = [
+        BaseAttribute('vn', 'm/s', 'Rated speed.'),
+        BaseAttribute('Fn', 'N', 'Rated force.'),
+        AliasAttribute('rated_speed', 'vn', groups=['rated_speed','rated']),
+        AliasAttribute('rated_force', 'Fn', groups=['rated_effort','rated']),
+        DerivedAttribute('Pn', 'W', 'Rated power', lambda vn, Fn: vn*Fn),
+        DerivedAttribute('fn', 'Hz', 'Rated frequency (electrical).', 
+                         lambda tau, vn: vn/(2*tau)),
+        AliasAttribute('rated_power', 'Pn', groups=['rated_power', 'rated']),
+        AliasAttribute('rated_frequency', 'fn', groups=['rated_speed','rated']),
+        # compatibility with rotary model
+        AliasAttribute('Tn', 'Fn', groups=['compatibility']),
+    ]
+    _DERIVATE_RULES = [
+        DerivateRule('vn', lambda Pn, Fn: Pn/Fn),
+        DerivateRule('vn', lambda tau, fn: 2*tau*fn),
+        DerivateRule('Fn', lambda vn, Pn: Pn/vn),
+        DerivateRule('Fm', lambda R, L, tau, vn, Pn, In, Un: flux_from_nominal_motor_mode(R, L, np.pi/tau, vn, Pn, In, Un, sign = 1, J = np.nan)[0]),
     ]
     _VALIDATORS = [
         Validator(lambda Un_rms, In_rms, Pn: 3*Un_rms*In_rms > Pn, 'Mechanical power output must not be greater then electrical power input')
